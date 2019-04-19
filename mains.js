9aba80450c153640832de00baf205c71b7b059e0
romain <romain.thomine007@gmail.com>
2019-04-19 10:01:44 +0200

    fini

---
+++ mains.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#btn1").click(function(){
+    $("p").css({'font-family': 'Arial'});
+  });
+  $("#btn2").click(function(){
+    $("p").css({'font-family': 'Montserra'});
+  });
+});
